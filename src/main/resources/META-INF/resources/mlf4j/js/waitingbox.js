cebc70a8a63273674308502d710c9a4344526a60
zollty <zollty@163.com>
2014-11-07 18:17:02 +0800

    html use zollty-temple and zollty-utils

--- src/main/resources/META-INF/resources/mlf4j/js/waitingbox.js
+++ src/main/resources/META-INF/resources/mlf4j/js/waitingbox.js
@@ -1,18 +1,50 @@
 /*!
- * @(#)waitingbox.js 
- *  A simple tool for waiting box
- * @athor zollty
- * @since 2013-11-10
+ * WaitingBox.js 
+ * A simple tool for waiting for page processing
+ * https://github.com/zollty-org/waitingbox
+ * Version 1.0 (Released on 2014-11-07)
+ * Licensed under the MIT license. Please see LICENSE for more information.
  */
-// ------------------------------------------------
+;(function(window){
 
-// /////////////waiting box SART////////////////
-var isWaitOnInit = false; //开关，如果设置为true，则会在文档加载时自动调用waiting box
+	
+//取得浏览器环境的waitingbox命名空间，非浏览器环境符合commonjs规范exports出去
+var waitingbox = typeof module === 'undefined' ? (window.waitingbox = window.waitingbox || {}) : module.exports;
 
-// 在页面上Write一个DIV
-document.write("<div id=\"waitingbox\" style=\"width:200px; height:60px; position:absolute; z-index:99; left:50%; top:50%; margin:-30px 0 0 -100px; border: 2px solid #CCE8CF; display:none;line-height: 60px; vertical-align: middle; text-align:center; z-index:100; font-size:12px; background-color:#F1FEDD; font-family:'微软雅黑';filter: alpha(opacity = 70);opacity: 0.7;\">正在处理中，请稍等 ...</div>");
 
-var wb_div = document.getElementById("waitingbox");
+var wb_div, isInit = false;
+
+/**
+ * @param [String] 开关，如果设置为true，则会在文档加载时自动调用waiting box
+ */
+function init(isWaitOnInit) {
+	
+	if(!isInit) {
+		var newElement = document.createElement("div");
+		newElement.setAttribute("id", "waitingbox");  
+		newElement.setAttribute("style", "width:200px; height:60px; position:absolute; z-index:99; left:50%; top:50%; margin:-30px 0 0 -100px; border: 2px solid #CCE8CF; display:none;line-height: 60px; vertical-align: middle; text-align:center; z-index:100; font-size:12px; background-color:#F1FEDD; font-family:'微软雅黑';filter: alpha(opacity = 70);opacity: 0.7;");  
+		var alertText = "正在处理中，请稍等 ..."; // Waiting processing please...
+		var newElementHtmlContent = document.createTextNode(alertText);
+		var bodyElement = document.getElementsByTagName('body')[0];
+		bodyElement.appendChild(newElement);
+		newElement.appendChild(newElementHtmlContent);
+		
+		wb_div = document.getElementById("waitingbox");
+		
+		if(isWaitOnInit){
+			showWBox();
+			if (document.all){//IE
+				window.attachEvent('onload',hideWBox);
+				window.attachEvent('onbeforeunload',showWBox);
+			}else{
+				window.addEventListener('load',hideWBox,false);
+				window.addEventListener('beforeunload',showWBox,false);
+			}
+		}
+	}
+	
+	isInit = true;
+}
 
 function zoutyMouseHandle(event) {
 	alert("处理未完成，请稍等 ...");
@@ -32,25 +64,26 @@ function zoutyReleaseMouseEvent() {
 		window.document.removeEventListener('mousedown', zoutyMouseHandle, false);
 	}
 }
+
+
 function hideWBox() {
+	if(!isInit) init(false);
 	zoutyReleaseMouseEvent();
 	document.body.style.cursor = "";
 	wb_div.style.display = "none";
 }
+
 function showWBox() {
 	zoutyBlockMouseEvent();
 	document.body.style.cursor = "wait";
+	if(!isInit) init(false);
 	wb_div.style.display = "";
 }
 
-if(isWaitOnInit){
-	showWBox();
-	if (document.all){//IE
-		window.attachEvent('onload',hideWaitingBox);
-		window.attachEvent('onbeforeunload',showWaitingBox);
-	}else{
-		window.addEventListener('load',hideWaitingBox,false);
-		window.addEventListener('beforeunload',showWaitingBox,false);
-	}
-}
+
+waitingbox.init = init;
+waitingbox.hide = hideWBox;
+waitingbox.show = showWBox;
 ///////////////waiting box END////////////////
+
+})(window);
